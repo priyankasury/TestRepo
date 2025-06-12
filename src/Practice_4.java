ee10aa659f7446b260750d2e1c5daf7414ec8167
priyankasury <suryawanshipriyanka98@gmail.com>
2025-06-12 15:52:00 +0530

    maximum number

---
+++ src/Practice_4.java
@@ -0,0 +1,10 @@
+public class Practice_4
+{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        int max= (a>b) ? a : b;
+        System.out.println(max);
+    }
+}
