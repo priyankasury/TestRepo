02ca30f63385c5c3e107b44bda5fc6e717a0fa19
priyankasury <suryawanshipriyanka98@gmail.com>
2025-06-12 16:28:07 +0530

    even and odd

--- src/Practice_5.java
+++ src/Practice_5.java
@@ -1,14 +1,15 @@
 public class Practice_5 {
     public static void main(String[] args) {
-        int A = 12;
-       // int B = 5;
+        int A = 17;
 
-        int a;
-        a = (A % 2);
-        if (a == 0) {
-            System.out.println("Even");
-        } else {
-            System.out.println("odd");
+       String Result  = (A%2==0) ? "Even" : "Odd";
+
+     //  String Result1= (B%2==1) ?"Even" :"Odd";
+
+            System.out.println(A + " is " +Result);
+
         }
     }
-}
+
+
+
