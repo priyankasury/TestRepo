6a8d0e5af29fdd0d9956c6d0bd317c98a9bc2a6e
priyankasury <suryawanshipriyanka98@gmail.com>
2025-06-12 15:46:43 +0530

    age

---
+++ src/Practice_3.java
@@ -0,0 +1,15 @@
+public class Practice_3
+{
+    public static void main(String[] args) {
+
+        int age =10;
+        String canivote = age>18 ? "Yes" : "No";
+
+        System.out.println(canivote);
+
+
+
+    }
+
+
+}
