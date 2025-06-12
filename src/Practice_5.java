3deff2b6c7d83f02f6e51e8460425f042fb6cd58
priyankasury <suryawanshipriyanka98@gmail.com>
2025-06-12 16:08:47 +0530

    even and odd

---
+++ src/Practice_5.java
@@ -0,0 +1,14 @@
+public class Practice_5 {
+    public static void main(String[] args) {
+        int A = 12;
+       // int B = 5;
+
+        int a;
+        a = (A % 2);
+        if (a == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
