e431247afe6f59eaea21ba97c124f4d0622e8099
priyankasury <suryawanshipriyanka98@gmail.com>
2025-06-15 16:55:27 +0530

    typecasting

--- src/Practice_6.java
+++ src/Practice_6.java
@@ -2,13 +2,13 @@ public class Practice_6
 {
     public static void main(String[] args) {
 
-        int marks=30;
+        int marks=90;
 
 
         String Grade= (marks>=90) ? "A+"
                      :(marks>=80) ? "A"
                     : (marks>=60) ? "B"
-                    : (marks<=40) ? "C" :"Fail" ;
+                    : (marks>=40) ? "C" :"Fail" ;
 
         System.out.println(Grade);
     }
---
+++ src/Practice_8.java
@@ -0,0 +1,10 @@
+public class Practice_8
+{
+    public static void main(String[] args)
+    {
+        int A=100;
+        Double a= (double)A;
+
+        System.out.println(a);
+    }
+}
