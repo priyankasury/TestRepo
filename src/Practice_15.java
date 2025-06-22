9c36bcafd1a645c6c0c2f04749a84e1965b5ba51
priyankasury <suryawanshipriyanka98@gmail.com>
2025-06-22 22:23:50 +0530

    greatest number

--- src/Practice_15.java
+++ src/Practice_15.java
@@ -8,10 +8,10 @@ public class Practice_15
         Scanner scanner=new Scanner(System.in);
 
         System.out.println("Enter the number1 =");
-        int a= scanner.nextInt();;
+        int a= scanner.nextInt();
 
         System.out.println("Enter the number2 =");
-        int b= scanner.nextInt();;
+        int b= scanner.nextInt();
 
         System.out.println("Enter the number3 =");
         int c= scanner.nextInt();
