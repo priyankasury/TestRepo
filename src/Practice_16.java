cc654287c0da3b2f5d10e2580b437f7fa4a9ff0a
priyankasury <suryawanshipriyanka98@gmail.com>
2025-06-22 22:35:57 +0530

    day

---
+++ src/Practice_16.java
@@ -0,0 +1,50 @@
+import java.lang.runtime.SwitchBootstraps;
+import java.util.Scanner;
+
+public class Practice_16
+{
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number =");
+        int i= scanner.nextInt();
+
+        switch(i)
+        {
+
+            case 1:
+           System.out.println("This is monday");
+           break;
+            case 2:
+                System.out.println("This is tuesday");
+                break;
+
+            case 3:
+                System.out.println("This is wednesday");
+                break;
+
+            case 4:
+                System.out.println("This is thursday");
+                break;
+
+            case 5:
+                System.out.println("This is friday");
+                break;
+
+            case 6:
+                System.out.println("This is saturday");
+                break;
+
+            case 7:
+                System.out.println("This is sunday");
+                break;
+
+            default:
+                System.out.println("This is none of day");
+        }
+
+
+
+    }
+}
